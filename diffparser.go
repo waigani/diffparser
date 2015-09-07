98cb937fc8adea154881a829c47272ef3da6025b
waigani <waigani@gmail.com>
2015-09-08 07:36:00 +1200

    Initial Commit
    Add basic functionality and unit tests. For now, renaming a file
    produces two files: one deleted, one modified. In the future, there
    will be one file with a mode of diffparser.RENAMED.

---
+++ diffparser.go
@@ -0,0 +1,235 @@
+package diffparser
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/juju/errors"
+)
+
+type FileMode int
+
+const (
+	DELETED FileMode = iota
+	MODIFIED
+	NEW
+)
+
+type diffRange struct {
+
+	// starting line number
+	Start int
+
+	// the number of lines the change diffHunk applies to
+	Length int
+
+	// Each line of the hunk range.
+	Lines []*DiffLine
+}
+
+type DiffLineMode int
+
+const (
+	ADDED DiffLineMode = iota
+	REMOVED
+	UNCHANGED
+)
+
+type DiffLine struct {
+	Mode    DiffLineMode
+	Number  int
+	Content string
+}
+
+type diffHunk struct {
+	OrigRange diffRange
+	NewRange  diffRange
+}
+
+type DiffFile struct {
+	Mode     FileMode
+	OrigName string
+	NewName  string
+	Hunks    []*diffHunk
+}
+
+type Diff struct {
+	Files []*DiffFile
+	Raw   string `sql:"type:text"`
+
+	PullID uint `sql:"index"`
+}
+
+func (d *Diff) addFile(file *DiffFile) {
+	d.Files = append(d.Files, file)
+}
+
+// Changed returns a map of filename to lines changed in that file. Deleted
+// files are ignored.
+func (d *Diff) Changed() map[string][]int {
+	dFiles := make(map[string][]int)
+
+	for _, f := range d.Files {
+		if f.Mode == DELETED {
+			continue
+		}
+
+		for _, h := range f.Hunks {
+			for _, dl := range h.NewRange.Lines {
+				if dl.Mode == ADDED {
+					dFiles[f.NewName] = append(dFiles[f.NewName], dl.Number)
+				}
+			}
+		}
+	}
+
+	return dFiles
+}
+
+func regFind(s string, reg string, group int) string {
+	re := regexp.MustCompile(reg)
+	return re.FindStringSubmatch(s)[group]
+}
+
+func lineMode(line string) (*DiffLineMode, error) {
+	var m DiffLineMode
+	switch line[:1] {
+	case " ":
+		m = UNCHANGED
+	case "+":
+		m = ADDED
+	case "-":
+		m = REMOVED
+	default:
+		return nil, errors.Errorf("could not parse line mode for line: %q", line)
+	}
+	return &m, nil
+}
+
+// Parse takes a diff, such as produced by "git diff", and parses it into a
+// Diff struct.
+func Parse(diffString string) (*Diff, error) {
+	var diff Diff
+	diff.Raw = diffString
+	lines := strings.Split(diffString, "\n")
+
+	var file *DiffFile
+	var hunk *diffHunk
+	var ADDEDCount int
+	var REMOVEDCount int
+	var inHunk bool
+	oldFilePrefix := "--- a/"
+	newFilePrefix := "+++ b/"
+
+	// Parse each line of diff.
+	for _, l := range lines {
+		switch {
+		case strings.HasPrefix(l, "diff "):
+			inHunk = false
+
+			// Start a new file.
+			file = &DiffFile{}
+			diff.Files = append(diff.Files, file)
+
+			// File mode.
+			file.Mode = MODIFIED
+		case l == "+++ /dev/null":
+			file.Mode = DELETED
+		case l == "--- /dev/null":
+			file.Mode = NEW
+		case strings.HasPrefix(l, oldFilePrefix):
+			file.OrigName = strings.TrimPrefix(l, oldFilePrefix)
+		case strings.HasPrefix(l, newFilePrefix):
+			file.NewName = strings.TrimPrefix(l, newFilePrefix)
+		case strings.HasPrefix(l, "@@ "):
+			inHunk = true
+			// Start new hunk.
+			hunk = &diffHunk{}
+			file.Hunks = append(file.Hunks, hunk)
+
+			// Parse hunk heading for ranges
+			re := regexp.MustCompile(`@@ \-(\d+),(\d+) \+(\d+),?(\d+)? @@`)
+			m := re.FindStringSubmatch(l)
+			a, err := strconv.Atoi(m[1])
+			if err != nil {
+				return nil, err
+			}
+			b, err := strconv.Atoi(m[2])
+			if err != nil {
+				return nil, err
+			}
+			c, err := strconv.Atoi(m[3])
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			d := c
+			if len(m[4]) > 0 {
+				d, err = strconv.Atoi(m[4])
+				if err != nil {
+					return nil, errors.Trace(err)
+				}
+			}
+
+			// hunk orig range.
+			hunk.OrigRange = diffRange{
+				Start:  a,
+				Length: b,
+			}
+
+			// hunk new range.
+			hunk.NewRange = diffRange{
+				Start:  c,
+				Length: d,
+			}
+
+			// (re)set line counts
+			ADDEDCount = hunk.NewRange.Start
+			REMOVEDCount = hunk.OrigRange.Start
+		case inHunk && isSourceLine(l):
+			m, err := lineMode(l)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			line := DiffLine{
+				Mode:    *m,
+				Content: l[1:],
+			}
+			newLine := line
+			origLine := line
+
+			// add lines to ranges
+			switch *m {
+			case ADDED:
+				newLine.Number = ADDEDCount
+				hunk.NewRange.Lines = append(hunk.NewRange.Lines, &newLine)
+				ADDEDCount++
+
+			case REMOVED:
+				origLine.Number = REMOVEDCount
+				hunk.OrigRange.Lines = append(hunk.OrigRange.Lines, &origLine)
+				REMOVEDCount++
+
+			case UNCHANGED:
+				newLine.Number = ADDEDCount
+				hunk.NewRange.Lines = append(hunk.NewRange.Lines, &newLine)
+				origLine.Number = REMOVEDCount
+				hunk.OrigRange.Lines = append(hunk.OrigRange.Lines, &origLine)
+				ADDEDCount++
+				REMOVEDCount++
+			}
+		}
+	}
+
+	return &diff, nil
+}
+
+func isSourceLine(line string) bool {
+	if line == `\ No newline at end of file` {
+		return false
+	}
+	if l := len(line); l == 0 || (l >= 3 && (line[:3] == "---" || line[:3] == "+++")) {
+		return false
+	}
+	return true
+}
---
+++ diffparser_test.go
@@ -0,0 +1,138 @@
+package diffparser_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	jt "github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	"github.com/waigani/diffparser"
+	gc "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+	gc.TestingT(t)
+}
+
+type suite struct {
+	jt.CleanupSuite
+	rawdiff string
+	diff    *diffparser.Diff
+}
+
+var _ = gc.Suite(&suite{})
+
+func (s *suite) SetUpSuite(c *gc.C) {
+	byt, err := ioutil.ReadFile("example.diff")
+	c.Assert(err, jc.ErrorIsNil)
+	s.rawdiff = string(byt)
+}
+
+// TODO(waigani) tests are missing more creative names (spaces, special
+// chars), and diffed files that are not in the current directory.
+
+func (s *suite) TestFileModeAndNaming(c *gc.C) {
+	diff, err := diffparser.Parse(s.rawdiff)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(diff.Files, gc.HasLen, 5)
+
+	for i, expected := range []struct {
+		mode     diffparser.FileMode
+		origName string
+		newName  string
+	}{
+		{
+			mode:     diffparser.MODIFIED,
+			origName: "file1",
+			newName:  "file1",
+		},
+		{
+			mode:     diffparser.DELETED,
+			origName: "file2",
+			newName:  "",
+		},
+		{
+			mode:     diffparser.DELETED,
+			origName: "file3",
+			newName:  "",
+		},
+		{
+			mode:     diffparser.NEW,
+			origName: "",
+			newName:  "file4",
+		},
+		{
+			mode:     diffparser.NEW,
+			origName: "",
+			newName:  "newname",
+		},
+	} {
+		file := diff.Files[i]
+		c.Logf("testing file: %v", file)
+		c.Assert(file.Mode, gc.Equals, expected.mode)
+		c.Assert(file.OrigName, gc.Equals, expected.origName)
+		c.Assert(file.NewName, gc.Equals, expected.newName)
+	}
+}
+
+func (s *suite) TestHunk(c *gc.C) {
+	diff, err := diffparser.Parse(s.rawdiff)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(diff.Files, gc.HasLen, 5)
+
+	expectedOrigLines := []diffparser.DiffLine{
+		{
+			Mode:    diffparser.UNCHANGED,
+			Number:  1,
+			Content: "some",
+		}, {
+			Mode:    diffparser.UNCHANGED,
+			Number:  2,
+			Content: "lines",
+		}, {
+			Mode:    diffparser.REMOVED,
+			Number:  3,
+			Content: "in",
+		}, {
+			Mode:    diffparser.UNCHANGED,
+			Number:  4,
+			Content: "file1",
+		},
+	}
+
+	expectedNewLines := []diffparser.DiffLine{
+		{
+			Mode:    diffparser.ADDED,
+			Number:  1,
+			Content: "add a line",
+		}, {
+			Mode:    diffparser.UNCHANGED,
+			Number:  2,
+			Content: "some",
+		}, {
+			Mode:    diffparser.UNCHANGED,
+			Number:  3,
+			Content: "lines",
+		}, {
+			Mode:    diffparser.UNCHANGED,
+			Number:  4,
+			Content: "file1",
+		},
+	}
+
+	file := diff.Files[0]
+	origRange := file.Hunks[0].OrigRange
+	newRange := file.Hunks[0].NewRange
+
+	c.Assert(origRange.Start, gc.Equals, 1)
+	c.Assert(origRange.Length, gc.Equals, 4)
+	c.Assert(newRange.Start, gc.Equals, 1)
+	c.Assert(newRange.Length, gc.Equals, 4)
+
+	for i, line := range expectedOrigLines {
+		c.Assert(*origRange.Lines[i], gc.DeepEquals, line)
+	}
+	for i, line := range expectedNewLines {
+		c.Assert(*newRange.Lines[i], gc.DeepEquals, line)
+	}
+}
